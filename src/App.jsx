427fbf1eeac33b246c2c9ce673c0e070d58c268c
CandyTale55 <candytale55@gmail.com>
2025-10-05 22:29:35 +0200

    Set initial styles and create App.jsx   * - Create a separate App component  * - Import and render the App component here  * - In the App component, render a <main> element  * - Style everything to look like the slide

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
-import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
+    <main>
       <h1>I am App (Tenzies)</h1>  
-    </>
+    </main>
   )
 }
 
