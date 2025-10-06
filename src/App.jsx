e354d71661a6bb1cc54e881c50cce1a3fa87b03d
CandyTale55 <candytale55@gmail.com>
2025-10-06 07:42:42 +0200

    refactor: update the array of numbers in state to be an array of objects

--- src/App.jsx
+++ src/App.jsx
@@ -9,23 +9,24 @@ export default function App() {
   function generateAllNewDice() {
     return new Array(10)
       .fill(0)
-      .map(() => Math.ceil(Math.random() * 6))
+      .map(() => ({
+        value: Math.ceil(Math.random() * 6),
+        isHeld: false
+      }))
   }
 
   function rollDice() {
     setDice(generateAllNewDice())
   }
 
-  const diceElements = dice.map(num => <Die value={num} />)
+  const diceElements = dice.map(num => <Die value={num.value} isHeld={num.isHeld} />)
 
   return (
     <main>
       <div className="dice-container">
         {diceElements}
       </div>
-
       <button className="roll-dice" onClick={rollDice}>Roll</button>
-
     </main>
   )
 }
