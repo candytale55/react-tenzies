bcbda25e396138afe794344037239b91d0492403
CandyTale55 <candytale55@gmail.com>
2025-10-06 06:05:24 +0200

    feat: add Die component and update App layout with dice container style: enhance index.css for improved layout and button styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
-import './App.css'
+import Die from "./components/Die";
 
-function App() {
 
+export default function App() {
   return (
     <main>
-      <h1>I am App (Tenzies)</h1>  
+      <div className="dice-container">
+        <Die value={1} />
+        <Die value={2} />
+        <Die value={3} />
+        <Die value={4} />
+        <Die value={5} />
+        <Die value={6} />
+        <Die value={1} />
+        <Die value={2} />
+        <Die value={3} />
+        <Die value={4} />
+      </div>
     </main>
   )
 }
-
-export default App
